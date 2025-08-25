2e5aac1ca299e67f66962b6e99fdff116b1544c0
Abdus Sobhan <128705141+SobhanLab@users.noreply.github.com>
2025-08-25 22:40:26 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,54 +2,68 @@ import SyntaxHighlighter from "./assets/syntaxhighlighter.js";
 
 let highlighter = SyntaxHighlighter;
 
-function highlight(){
+// Function to highlight code
+function highlight() {
   let lang = $("#select").val();
   let input = $("#input textarea").val();
+
   console.log("Language: " + lang + "\nInput: " + input);
+
   $("#result").html(`
-        <pre class="brush: ${lang}">${input}</pre>
-    `);
+    <pre class="brush: ${lang}">${input}</pre>
+  `);
+
   highlighter();
-  
-  //Save language
+
+  // Save selected language in cookie
   document.cookie = "lang=" + lang + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
 }
 
+// Copy highlighted code to clipboard
 $("#copy").click(function () {
-  var range = document.createRange();
-  range.selectNode($("#result .syntaxhighlighter")[0]);
-  window.getSelection().removeAllRanges(); // clear current selection
-  window.getSelection().addRange(range); // to select text
+  const codeElement = $("#result .syntaxhighlighter")[0];
+  if (!codeElement) return; // nothing to copy
+
+  const textToCopy = codeElement.innerText;
+
+  navigator.clipboard.writeText(textToCopy)
+    .then(() => {
+      alert("Code copied to clipboard!");
+    })
+    .catch((err) => {
+      console.error("Failed to copy: ", err);
+    });
 });
 
+// Highlight on textarea input
 $("#input textarea").on("change keyup paste", function () {
   highlight();
 });
 
+// Highlight on language selection change
 $("#select").change(function () {
   highlight();
 });
 
+// Helper function to get cookie value
 function getCookie(name) {
-  const cookieString = document.cookie;
-  const cookies = cookieString.split(";");
-
+  const cookies = document.cookie.split(";");
   for (let i = 0; i < cookies.length; i++) {
     const cookie = cookies[i].trim();
     if (cookie.startsWith(name + "=")) {
       return cookie.substring(name.length + 1);
     }
   }
-
   return null;
 }
 
-$(document).ready(function() {
-  // Retrieve the stored language cookie
-  let storedLang = getCookie("lang");
+// Restore selected language from cookie
+$(document).ready(function () {
+  const storedLang = getCookie("lang");
   if (storedLang) {
-    // Set the value of the select element to the stored language
     $("#select").val(storedLang);
   }
-});
 
+  // Initial highlight
+  highlight();
+});
